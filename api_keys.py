4c3bf04225f68a54cfdf2b20b0cd023994b00a15
Andrew Cliffe <acliffe45@gmail.com>
2020-07-13 23:25:48 -0700

    Adding mostly completed VacationPy.ipynb; hotels piece still has issues

---
+++ api_keys.py
@@ -0,0 +1,5 @@
+# My API Keys
+g_key = "AIzaSyBuPCsXBjtu6wpy_QcmEeClh1ZH_E2Is7c"
+c_key = "5a1b63f57a7821759aba828c2962c6ccebcb6b01"
+weather_api_key = "093e43ab84cdd692e1468f48df6faab0"
+omdb_key = "7499936f"
